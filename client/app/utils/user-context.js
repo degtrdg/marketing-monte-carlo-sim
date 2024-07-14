f4e8cfe6096400a730c5fc4a347477a00767358f
Raghav Pillai <76788658+raghavpillai@users.noreply.github.com>
2024-07-13 19:56:09 -0700

    Update sidebar and user context files

--- client/app/utils/user-context.js
+++ client/app/utils/user-context.js
@@ -28,13 +28,13 @@ export function UserProvider({ children }) {
   //   // }, 1000);
   // };
 
-  const searchCompany = async (name, companyInfo, post) => {
+  const searchCompany = async (name, companyInfo) => {
     setCompanyName(name);
     setCompanyInfo(null); // Reset company info while fetching
 
     try {
       const response = await fetch(
-        `https://localhost:8080/api/fetch-all-company-information?company=${encodeURIComponent(
+        `http://localhost:8080/api/fetch-all-company-information?company=${encodeURIComponent(
           name
         )}`,
         {
@@ -53,14 +53,61 @@ export function UserProvider({ children }) {
       console.log("name", data.company_leaders[0].organization.name);
       console.log("description", data.company_leaders[0].organization.industry);
 
+      let employees = [];
+      console.log(data);
+      for (const employee in data.company_leaders) {
+        employees.push({
+          name: employee.name,
+          title: employee.title,
+          description: "",
+        });
+      }
+
+      sales_pitch = [
+        "Congrats on raising $500k in your Pre-Seed funding round last year! I'm impressed with how Speck is solving everyday workplace challenges efficiently.",
+        "With such growth, managing finances can become tricky. Are you finding it hard to keep your burn rate under control?",
+        "At Hiline, we help over 300 businesses keep their finances in check. We handle daily bookkeeping, monthly reports, and payroll. We even helped Jahnel Group save $1M in taxes.",
+        "Is this something you're dealing with? If so, just hit reply and let's chat.",
+      ];
+
+      const getPersonInfo = async (person) => {
+        const response = await fetch(
+          `http://localhost:8080/api/simulate-sales-pitch`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              company_info: {
+                company_name: data.company_name,
+                company_description: data.description,
+              },
+              person: person,
+              sales_pitch: sales_pitch,
+            }),
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+      };
+      let found_employees = [];
+      for (const person of employees) {
+        info = getPersonInfo(person);
+        if (info) {
+          found_employees.push(info);
+        }
+      }
+
       setCompanyInfo({
         name: data.company_name,
-        industry: data.industry || "Not available",
-        size: data.estimated_num_employees
-          ? `${data.estimated_num_employees} employees`
-          : "Not available",
-        description: data.company_summary || "No description available",
-        post: post,
+        industry: data.industry,
+        tagline: data.tag_line,
+        description: data.description,
+        company_logo: data.logo_url,
+        company_url: data.company_url,
+        employees: data.employees,
+        employee_list: found_employees,
         companyInfo: {
           ...data,
           ...companyInfo, // Merge with any existing companyInfo
@@ -74,7 +121,6 @@ export function UserProvider({ children }) {
         industry: "Information not available",
         size: "Information not available",
         description: "Failed to fetch company information",
-        post: post,
         companyInfo: companyInfo,
       });
     }
